e512203ed90ba2df8eff14873c131cac0c98803c
agoldh20 <a.goldh20@gmail.com>
2018-02-10 22:43:49 -0600

    pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,12 +1,27 @@
 class Pangram
   def self.empty_string(string)
-    @string = string.strip
-    @string.length == 0
+    string = string.strip
+    string.length == 0
+  end
+    
+  def self.pangram_tester(string)
+      stripped = string.strip.downcase
+      stripped.gsub!(/[^a-z]/,'')
+      array = stripped.chars
+      return true if array.uniq.count >= 26
   end
 
   def self.pangram?(phrase)
     if empty_string(phrase)
       return false
-    end
+    elsif pangram_tester(phrase) == true
+      return true
+    else
+      false
+    end 
   end
 end
+
+module BookKeeping
+    VERSION = 6 
+  end
