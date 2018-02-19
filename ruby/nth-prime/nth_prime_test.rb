f364801ed9ac7f20184464a2351b893f97574317
agoldh20 <a.goldh20@gmail.com>
2018-02-18 20:34:20 -0600

    leap

--- ruby/nth-prime/nth_prime_test.rb
+++ ruby/nth-prime/nth_prime_test.rb
@@ -46,7 +46,7 @@ class NthPrimeTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
