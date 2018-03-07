25e50802fb6f004a707ae1852793f104dec6ef73
agoldh20 <a.goldh20@gmail.com>
2018-03-06 19:00:16 -0600

    word count

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,27 @@
+class Phrase
+  def initialize(string)
+    @string = string.downcase.tr(","," ").split(" ")
+    @string.map! do |word|
+      if word.start_with?("'") && word.end_with?("'")
+        word.tr("'","")
+      elsif !word.include?("'") 
+        word.gsub(/\W+/, '')
+      else
+        word
+      end
+    end
+    p @string
+  end
+
+  def word_count
+    counter = {}
+    @string.each do |word, count|
+      counter["#{word}".to_sym.to_s] = @string.count(word)
+    end
+    counter
+  end
+end
+
+module BookKeeping
+   VERSION = 1 # Where the version number matches the one in the test.
+end
