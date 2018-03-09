aab52667b82b688c334a1e15898771f236fc3a82
agoldh20 <a.goldh20@gmail.com>
2018-03-09 16:04:29 -0600

    run_length_encoding

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -6,16 +6,16 @@ class RunLengthEncoding
     output = ""
     start = 0
     key.each do |key_letter|
-      count = 0
+      code_counter = 0
       original.slice(start..-1).each do |original_letter|
         if original_letter == key_letter
-          count += 1
+          code_counter += 1
         else 
           break
         end
       end
-      start += count
-      output << "#{count if count > 1}#{key_letter}"
+      start += code_counter
+      output << "#{code_counter if code_counter > 1}#{key_letter}"
     end
     output
   end
