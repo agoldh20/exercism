3aaf2f2cdcb669cd63915087bd847b8bdc57b7c6
agoldh20 <dmgldwtr@gmail.com>
2019-03-26 15:22:12 -0500

    linked lists and started hexadecimal

---
+++ ruby/hexadecimal/hexadecimal.rb
@@ -0,0 +1,35 @@
+class Hexadecimal
+  def initialize(string)
+    @string = string
+  end
+
+  def to_decimal
+    hex_key = {"0" => 0,
+               "1" => 1,
+               "2" => 2,
+               "3" => 3,
+               "4" => 4,
+               "5" => 5,
+               "6" => 6,
+               "7" => 7,
+               "8" => 8,
+               "9" => 9,
+               "a" => 10,
+               "b" => 11,
+               "c" => 12,
+               "d" => 13,
+               "e" => 14,
+               "f" => 15}
+    parsed = @string.chars
+    output = []
+    valid = nil
+
+    parsed.each {|element| valid = !!hex_key.has_key?(element) && output << hex_key[element]}
+
+    unless valid
+      0
+    else
+
+    end
+  end
+end
