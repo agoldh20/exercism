b5f58cbd036ac8171a68b33486348a9d3319400d
Adam Goldwater <dmgldwtr@gmail.com>
2019-07-10 08:50:31 -0500

    WIP saddle points

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -7,33 +7,45 @@ class Matrix
     @columns = []
   end
 
-  def convert
+  def rows
     @matrix.each do |row|
       @rows << row.split(" ").map! {|number| number.to_i}
     end
-  end
-
-  def rows
-    convert
     @rows
   end
   
   def columns
-    convert
+    rows
     @rows[0].length.times {@columns << []}
     @rows.each do |row|
-      index = 0
-      row.each do |number|
+      row.each_with_index do |number, index|
         @columns[index] << number
-        index += 1
       end
-      index = 0
     end
     @columns
   end
   
   def saddle_points
-
+    saddle_points = []
+    columns
+    @rows.each_with_index do |row, index|
+      row_max = 0
+      col_min = @columns[0][0]
+      row_max_position = nil
+      col_min_position = nil
+      row.each do |row_number|
+        row_max = row_number
+        row_max_position = row.find_index(row_number)
+        @columns[index].each do |col_number|
+          col_min = col_number if col_min >= col_number
+          col_min_position = @columns[index].find_index(col_number)
+        end
+        puts "row_max: #{row_max}, col_min: #{col_min}"
+        if row_max == col_min
+          saddle_points << [row_max_position, col_min_position]
+        end
+      end
+    end
+    saddle_points
   end
-  
 end
